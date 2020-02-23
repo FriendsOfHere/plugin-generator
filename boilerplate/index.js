744dae9cbd123d08a875ef4b9a1efbedb6c2da3e
lifesign <leocloudy@gmail.com>
2020-02-23 23:40:51 +0800

    format

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -13,26 +13,26 @@ const net = require("net")
 function updateData() {
 
 <% if (renderComponents.includes('mini')) { %>    //miniWindow    
-    here.setMiniWindow({ title: "Hello MiniWindow", detail: "This is a demo here plugin" })<% } %>
+  here.setMiniWindow({ title: "Hello MiniWindow", detail: "This is a demo here plugin" })<% } %>
 <% if (renderComponents.includes('menuBar')) { %>    // Menu Bar
-    here.setMenuBar({ title: "Hello Menu Bar"})<% } %>
+  here.setMenuBar({ title: "Hello Menu Bar"})<% } %>
 <% if (renderComponents.includes('dock')) { %>    // Dock
-    here.setDock({
-        title: "Hello",
-        detail: "Dock"
-    })<% } %>
+  here.setDock({
+    title: "Hello",
+    detail: "Dock"
+  })<% } %>
 
 }
 
 here.onLoad(() => {
-    updateData()
-    // Update every 2 hours
-    setInterval(updateData, 2*3600*1000);
+  updateData()
+  // Update every 2 hours
+  setInterval(updateData, 2*3600*1000);
 })
 
 net.onChange((type) => {
-    console.log("Connection type changed:", type)
-    if (net.isReachable()) {
-        updateData()
-    }
+  console.log("Connection type changed:", type)
+  if (net.isReachable()) {
+    updateData()
+  }
 })
